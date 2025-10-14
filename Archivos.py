f5b36489da4994cf5114e5a6d22d9ce8af83523d
josueMugas <josue.mugaset32@gmail.com>
2025-10-14 16:35:31 -0300

    Add files via upload

---
+++ Archivos.py
@@ -0,0 +1,28 @@
+import random
+palabrasPosibles=["pepe","lapiz","cebolla","raul","acdc",]
+indicePalabra=random.randint(0,4)
+palabraAdivinar=palabrasPosibles[indicePalabra]
+palabra_=[]
+intentos=1
+cantidadLetras=len(palabraAdivinar)
+for x in range(cantidadLetras):
+    palabra_.append("_")
+while True:
+    print(palabra_,intentos)
+    palabraUsuario=input("ingrese una letra: ")
+    acierto= False
+    for Indiceletra in range(len(palabraAdivinar)):
+        for letraUsuario in palabraUsuario:
+            if letraUsuario==palabraAdivinar[Indiceletra]:
+                palabra_[Indiceletra]=letraUsuario
+                acierto = True
+    if not acierto:
+        intentos += 1
+    if not ("_" in palabra_):
+        print(f"ganaste!! en {intentos} intentos")
+        break
+    elif intentos==7:
+        print(f"perdiste:( la palabra era {palabraAdivinar}")
+        break
+with open("resultado.txt","w",encoding="utf-8") as archivo:
+    archivo.write(f"la palabra era {palabraAdivinar} y la adivinaste en {intentos} intentos\n")
